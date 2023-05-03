d1d5563f6f91b51e82ea5f520a0c378dfb616362
Christopher Wong <cw30355@gmail.com>
2023-05-03 00:05:37 -0400

    hooked up tailwind dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './input.css'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 // import { ContextProvider } from './contexts/ContextProvider'; 
