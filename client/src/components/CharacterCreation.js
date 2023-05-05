3b0b7dd0fe35d2b13344d48e7a1b1a3226f55f55
Christopher Wong <cw30355@gmail.com>
2023-05-04 23:15:22 -0400

    finished character creation component

--- client/src/components/CharacterCreation.js
+++ client/src/components/CharacterCreation.js
@@ -5,8 +5,12 @@ import Trait from "./Trait";
 
 function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
     const classes = [{'name': 'Barbarian','subclass': 'Berserker'},{'name': 'Bard','subclass': 'Lore'},{'name': 'Cleric','subclass': 'Life'},{'name': 'Druid','subclass': 'Land'},{'name': 'Fighter','subclass': 'Champion'},{'name': 'Monk','subclass': 'Open-Hand'},{'name': 'Paladin','subclass': 'Devotion'},{'name': 'Ranger','subclass': 'Hunter'},{'name': 'Rogue','subclass': 'Thief'},{'name': 'Sorcerer','subclass': 'Draconic'},{'name': 'Warlock','subclass': 'Fiend'},{'name': 'Wizard','subclass': 'Evocation'}]
-    const races = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Half-Orc', 'Halfing', 'Human', 'Tiefling']
+    const races = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Half-Orc', 'Halfling', 'Human', 'Tiefling']
+    const [characterName, setCharacterName] = useState('')
+    const [characterBio, setCharacterBio] = useState('')
     const [selectedClass, setSelectedClass] = useState('')
+    const [passivePerception, setPassivePerception] = useState(0)
+    const [ac, setAc] = useState(0)
     const [classData, setClassData] = useState('')
     const [featureData, setFeatureData] = useState('')
     const [checkedSkill, setCheckedSkill] = useState({})
@@ -27,15 +31,14 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
     const [wisdomAS, setWisdomAS] = useState(0)
     const [charismaAS, setCharismaAS] = useState(0)
     const [asBonus, setAsBonus] = useState({'str':0, 'dex':0, 'con':0, 'int':0, 'wis':0, 'cha':0})
-    // console.log(selectedClass)
-    // console.log(classData)
-    // console.log(featureData)
-    // console.log(selectedSubclass)
-    // console.log(subclassData)
-    // console.log(selectedRace)
-    // console.log(raceData)
-    // console.log(checkedSkill)
-    // console.log(prof)
+    const [checkedASB, setCheckedASB] = useState({})
+    const [hitDie, setHitDie] = useState('')
+    const [hitPoints, setHitPoints] = useState(0)
+    const [ogHitPoints, setOgHitPoints] = useState(0)
+    const [abProf, setAbProf] = useState({'str':false, 'dex':false, 'con':false, 'int':false, 'wis':false, 'cha':false})
+    const [pb, setPb] = useState(2)
+    const [level, setLevel] = useState(1)
+    const [spellcasting, setSpellcasting] = useState({'scab':0, 'scat':0, 'scs':0})
 
     useEffect(()=>{
         fetch(`https://www.dnd5eapi.co/api/classes/${selectedClass.toLowerCase()}`)
@@ -43,6 +46,9 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
         .then((data) => {
             setClassData(data)
             setClassProf(data.proficiencies)
+            setHitDie(data.hit_die)
+            setOgHitPoints(Math.floor(Math.random()*data.hit_die)+1)
+            setAbProf(addAbProf(data))
         })
     },[selectedClass])
 
@@ -71,6 +77,47 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
         })
     },[selectedRace])
 
+    useEffect(()=>{
+        setHitPoints(ogHitPoints + Math.floor((asBonus['con'] + constitutionAS - 10)/2) > 0 ? ogHitPoints + Math.floor((asBonus['con'] + constitutionAS - 10)/2): 1)
+    },[asBonus['con'], constitutionAS, selectedClass])
+
+    useEffect(()=>{
+        setPassivePerception(asBonus['wis']+wisdomAS-10 > 0 ? Math.floor((asBonus['wis'] + wisdomAS - 10)/2)+10: Math.ceil((asBonus['wis'] + wisdomAS - 10)/2)+10)
+    },[asBonus['wis'], wisdomAS, selectedRace])
+
+    useEffect(()=>{
+        setAc(asBonus['dex']+dexterityAS-10 > 0 ? Math.floor((asBonus['dex'] + dexterityAS - 10)/2)+10: Math.ceil((asBonus['dex'] + dexterityAS - 10)/2)+10)
+    },[asBonus['dex'], dexterityAS], selectedRace)
+
+    useEffect(()=>{
+        if(classData && classData.spellcasting){
+            // (()=>{
+                switch(classData.spellcasting.spellcasting_ability.index){
+                    case 'cha':
+                        setSpellcasting({
+                            'scab': asBonus['cha']+charismaAS-10 > 0 ? Math.floor((asBonus['cha'] + charismaAS - 10)/2): Math.ceil((asBonus['cha'] + charismaAS - 10)/2),
+                            'scat': asBonus['cha']+charismaAS-10 > 0 ? Math.floor((asBonus['cha'] + charismaAS - 10)/2)+pb: Math.ceil((asBonus['cha'] + charismaAS - 10)/2)+pb,
+                            'scs': asBonus['cha']+charismaAS-10 > 0 ? Math.floor((asBonus['cha'] + charismaAS - 10)/2)+pb+8: Math.ceil((asBonus['cha'] + charismaAS - 10)/2)+pb+8
+                        })
+                        break
+                    case 'wis':
+                        setSpellcasting({
+                            'scab': asBonus['wis']+wisdomAS-10 > 0 ? Math.floor((asBonus['wis'] + wisdomAS - 10)/2): Math.ceil((asBonus['wis'] + wisdomAS - 10)/2),
+                            'scat': asBonus['wis']+wisdomAS-10 > 0 ? Math.floor((asBonus['wis'] + wisdomAS - 10)/2)+pb: Math.ceil((asBonus['wis'] + wisdomAS - 10)/2)+pb,
+                            'scs': asBonus['wis']+wisdomAS-10 > 0 ? Math.floor((asBonus['wis'] + wisdomAS - 10)/2)+pb+8: Math.ceil((asBonus['wis'] + wisdomAS - 10)/2)+pb+8
+                        })
+                        break
+                    case 'int':
+                        setSpellcasting({
+                            'scab': asBonus['int']+intelligenceAS-10 > 0 ? Math.floor((asBonus['int'] + intelligenceAS - 10)/2): Math.ceil((asBonus['int'] + intelligenceAS - 10)/2),
+                            'scat': asBonus['int']+intelligenceAS-10 > 0 ? Math.floor((asBonus['int'] + intelligenceAS - 10)/2)+pb: Math.ceil((asBonus['int'] + intelligenceAS - 10)/2)+pb,
+                            'scs': asBonus['int']+intelligenceAS-10 > 0 ? Math.floor((asBonus['int'] + intelligenceAS - 10)/2)+pb+8: Math.ceil((asBonus['int'] + intelligenceAS - 10)/2)+pb+8
+                        })
+                        break
+            }
+        }
+    },[asBonus['cha'], asBonus['wis'], asBonus['int'], charismaAS, wisdomAS, intelligenceAS])
+    
     function handleClassSelection(e){
         setSelectedClass(e.target.value)
         let subclass = classes.filter((characterClass)=>characterClass.name===e.target.value)[0].subclass
@@ -109,7 +156,15 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
         return removedProf
     }
     function handleProfChoiceChange(e){
-        e.target.checked ? setCheckedProf({...checkedEquip, [e.target.name]:e.target.checked}): setCheckedProf(removeProfChoice(e.target.name))
+        e.target.checked ? setCheckedProf({...checkedProf, [e.target.name]:e.target.checked}): setCheckedProf(removeProfChoice(e.target.name))
+    }
+    function removeASBChoice(choice){
+        const {[choice]: value, ...removedASB} = checkedASB
+        return removedASB
+    }
+    function handleASBChoiceChange(e, bonus){
+        e.target.checked ? setCheckedASB({...checkedASB, [e.target.name]:e.target.checked}): setCheckedASB(removeASBChoice(e.target.name))
+        e.target.checked ? setAsBonus({...asBonus, [e.target.name.toLowerCase()]: (asBonus[e.target.name.toLowerCase()] + bonus)}):setAsBonus({...asBonus, [e.target.name.toLowerCase()]: (asBonus[e.target.name.toLowerCase()] - bonus)})
     }
     function handleReroll(e){
         e.preventDefault()
@@ -158,13 +213,52 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
         }
         return(resetASB)
     }
+    function addAbProf(data){
+        let resetAbProf = {'str':false, 'dex':false, 'con':false, 'int':false, 'wis':false, 'cha':false}
+        if(data){
+            data.saving_throws.map((abp)=>{
+                switch(abp.index){
+                    case 'str':
+                        resetAbProf['str']=true
+                        break
+                }
+                switch(abp.index){
+                    case 'dex':
+                        resetAbProf['dex']=true
+                        break
+                }
+                switch(abp.index){
+                    case 'con':
+                        resetAbProf['con']=true
+                        break
+                }
+                switch(abp.index){
+                    case 'int':
+                        resetAbProf['int']=true
+                        break
+                }
+                switch(abp.index){
+                    case 'wis':
+                        resetAbProf['wis']=true
+                        break
+                }
+                switch(abp.index){
+                    case 'cha':
+                        resetAbProf['cha']=true
+                        break
+                }
+            }) 
+        }
+        return(resetAbProf)
+    }
+
 
     return(
         <div>
             <div>Character Creation</div>
             <form >
-                <input type='text' placeholder="Character name" className="block w-1/2 px-4 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></input>
-                <input type='text' placeholder="Character bio" className="block w-1/2 px-4 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></input>
+                <input type='text' onChange={(e)=>setCharacterName(e.target.value)} placeholder="Character name" className="block w-1/2 px-4 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></input>
+                <input type='text' onChange={(e)=>setCharacterBio(e.target.value)} placeholder="Character bio" className="block w-1/2 px-4 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></input>
                 <select id="class-selection" name="class-selection" onChange={handleClassSelection}>
                     <option value={''} disabled selected>Select a Class</option>
                     {classes.map((characterClass, index)=><option key={index} value={characterClass.name}>{characterClass.name}</option>)}
@@ -174,13 +268,13 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
                     <option value={''} disabled selected>Select a Race</option>
                     {races.map((race, index)=><option key={index} value={race}>{race}</option>)}
                 </select>
-                <div>Level: 1</div>
-                <div>Proficiency Bonus: +2 </div>
-                <div>Passive Perception: </div>
-                <div>Speed: </div>
-                <div>Armor Class: </div>
-                <div>Hit Points: </div>
-                <div>Hit Die: </div>
+                <div>Level: {level}</div>
+                <div>Proficiency Bonus: +{pb} </div>
+                <div>Passive Perception: {passivePerception} </div>
+                <div>Speed: {raceData.speed} </div>
+                <div>Armor Class: {ac} </div>
+                <div>Hit Points: {hitPoints && hitPoints > 0 ? hitPoints:('')}</div>
+                <div>Hit Die: {hitDie}</div>
                 <div>Strength: {strengthAS + asBonus['str']}</div>
                 <div>Dexterity: {dexterityAS + asBonus['dex']}</div>
                 <div>Constitution: {constitutionAS + asBonus['con']}</div>
@@ -188,18 +282,39 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
                 <div>Wisdom: {wisdomAS + asBonus['wis']}</div>
                 <div>Charisma: {charismaAS + asBonus['cha']}</div>
                 <button onClick={handleReroll}>Roll Ability Scores</button>
-                <div>Strength Proficiency: </div>
-                <div>Dexterity Proficiency: </div>
-                <div>Constitution Proficiency: </div>
-                <div>Intelligence Proficiency: </div>
-                <div>Wisdom Proficiency: </div>
-                <div>Charisma Proficiency: </div>
-                <div>Strength Saving Throw: </div>
-                <div>Dexterity Saving Throw: </div>
-                <div>Constitution Saving Throw: </div>
-                <div>Intelligence Saving Throw: </div>
-                <div>Wisdom Saving Throw: </div>
-                <div>Charisma Saving Throw: </div>
+                {raceData && raceData.ability_bonus_options ? (
+                    <div>
+                        <div>Choose {raceData.ability_bonus_options.choose} of the following ability score bonuses:</div>
+                        {raceData.ability_bonus_options.from.options.map((option, index)=>
+                            <div key={index}>
+                                <input type="checkbox" name={option.ability_score.name} disabled={Object.keys(checkedASB).length >= raceData.ability_bonus_options.choose && !checkedASB[`${option.ability_score.name}`]} onChange={(e) => handleASBChoiceChange(e, option.bonus)} />
+                                <label>{option.ability_score.name}: +{option.bonus}</label>
+                            </div>
+                        )}
+                    </div>
+                ):('')}
+                <div>Ability Proficiencies:</div>
+                {abProf['str'] ? <div>Strength </div>:null}
+                {abProf['dex'] ? <div>Dexterity </div>:null}
+                {abProf['con'] ? <div>Constitution </div>:null}
+                {abProf['int'] ? <div>Intelligence </div>:null}
+                {abProf['wis'] ? <div>Wisdom </div>:null}
+                {abProf['cha'] ? <div>Charisma </div>:null}
+                <div>Saving Throws</div>
+                <div>Strength Saving Throw: {asBonus['str']+strengthAS-10 > 0 ? '+' + Math.floor((asBonus['str'] + strengthAS - 10)/2): Math.ceil((asBonus['str'] + strengthAS - 10)/2)}</div>
+                <div>Dexterity Saving Throw: {asBonus['dex']+dexterityAS-10 > 0 ? '+' + Math.floor((asBonus['dex'] + dexterityAS - 10)/2): Math.ceil((asBonus['dex'] + dexterityAS - 10)/2)}</div>
+                <div>Constitution Saving Throw: {asBonus['con']+constitutionAS-10 > 0 ? '+' + Math.floor((asBonus['con'] + constitutionAS - 10)/2): Math.ceil((asBonus['con'] + constitutionAS - 10)/2)}</div>
+                <div>Intelligence Saving Throw: {asBonus['int']+intelligenceAS-10 > 0 ? '+' + Math.floor((asBonus['int'] + intelligenceAS - 10)/2): Math.ceil((asBonus['int'] + intelligenceAS - 10)/2)}</div>
+                <div>Wisdom Saving Throw: {asBonus['wis']+wisdomAS-10 > 0 ? '+' + Math.floor((asBonus['wis'] + wisdomAS - 10)/2): Math.ceil((asBonus['wis'] + wisdomAS - 10)/2)}</div>
+                <div>Charisma Saving Throw: {asBonus['cha']+charismaAS-10 > 0 ? '+' + Math.floor((asBonus['cha'] + charismaAS - 10)/2): Math.ceil((asBonus['cha'] + charismaAS - 10)/2)}</div>
+                {classData && classData.spellcasting ? (
+                    <div>
+                        <div>Spellcasting</div>
+                        <div>SpellCasting Ability: {spellcasting['scab']} </div>
+                        <div>Spellcasting Attack: {spellcasting['scat']} </div>
+                        <div>Spellcasting Save: {spellcasting['scs']} </div>
+                    </div>
+                ):null}
                 <div>
                     <div>Features</div>
                     {featureData ? (
@@ -208,7 +323,7 @@ function CharacterCreation({setFeatureUrl, setProficiencyUrl, setTraitUrl}){
                                 <Feature key={index} name={feature.name} url={feature.url} setFeatureUrl={setFeatureUrl} />
                             )}
                         </div>
-                    ):('')}
+                    ):null}
                 </div>
                 <div>
                     <div>Traits</div>
